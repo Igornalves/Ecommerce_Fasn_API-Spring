7e2aa63ceabf46c04a8e094ac473f9508b2e8138
igor_alves <igornalves08@gmail.com>
2024-10-12 17:44:54 -0300

    colocando novas coisas no projeto completo como rota de test

--- src/main/java/com/ecommerce/fashionshop/FashionshopApplication.java
+++ src/main/java/com/ecommerce/fashionshop/FashionshopApplication.java
@@ -3,11 +3,13 @@ package com.ecommerce.fashionshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FashionshopApplication {
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Ecommerce para Loja de Roupa", description = "API responsavel por vendas e compras de produtos", version = "1.0"))
+public class FashionshopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FashionshopApplication.class, args);
 	}
-
 }
